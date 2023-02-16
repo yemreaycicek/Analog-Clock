c7e5125396f74d6f7c871133e730d1e0b5060552
Yunus Emre AYÇİÇEK <yunus.e.aycicek@gmail.com>
2023-02-16 04:03:15 +0300

    Project finished.

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const hr = document.querySelector(".hr");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
+
+setInterval(() => {
+   const date = new Date();
+   const hh = date.getHours() * 30;
+   const mm = date.getMinutes() * 6;
+   const ss = date.getSeconds() * 6;
+
+   hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+   min.style.transform = `rotateZ(${mm}deg`;
+   sec.style.transform = `rotateZ(${ss}deg`;
+});
